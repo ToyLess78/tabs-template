e7a1d7da7cf57c863ba866066568e7236c5530f4
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-09-18 23:07:41 +0300

    refactor: add router basename

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,19 +4,24 @@ import { Layout } from "../libs/components/components";
 import { AppRoute, TabName } from "../libs/enums/enums";
 import { pages } from "../pages/pages";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+	[
+		{
+			path: AppRoute.DASHBOARD,
+			element: <Layout />,
+			children: [
+				...Object.keys(TabName).map((tabName) => ({
+					path: AppRoute[tabName as keyof typeof TabName],
+					element: React.createElement(pages[tabName as keyof typeof TabName]),
+				})),
+				{
+					path: AppRoute.ANY,
+					element: <Navigate to={AppRoute.DASHBOARD} />,
+				},
+			],
+		},
+	],
 	{
-		path: AppRoute.DASHBOARD,
-		element: <Layout />,
-		children: [
-			...Object.keys(TabName).map((tabName) => ({
-				path: AppRoute[tabName as keyof typeof TabName],
-				element: React.createElement(pages[tabName as keyof typeof TabName]),
-			})),
-			{
-				path: AppRoute.ANY,
-				element: <Navigate to={AppRoute.DASHBOARD} />,
-			},
-		],
+		basename: "/tabs-template",
 	},
-]);
+);
